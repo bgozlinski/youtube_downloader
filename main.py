22ca4e2c36ee77e447bda4203b67a93cdf7b5a9f
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-07-24 20:54:06 +0200

    Added progress bar

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from pytube import YouTube
 def progress_function(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
-
     percentage_of_completion = bytes_downloaded / total_size * 100
-    print(f"\rDownloading... {percentage_of_completion:.2f}% complete", end='')
+
+    bar_length = 50
+    filled_bar = int(percentage_of_completion * bar_length / 100)
+    bar = '=' * filled_bar + ' ' * (bar_length - filled_bar)
+
+    print(f"\r[{bar}] Downloading... {percentage_of_completion:.2f}% complete", end='')
 
 
 def download_video(url, save_path="."):
