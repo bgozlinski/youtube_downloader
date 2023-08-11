e5f310bd6f2f712fca33cd7c22dd0afe4eff0cd0
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-08-11 20:36:05 +0200

    Add download speed display to YouTube downloader's progress bar
    - Enhanced the progress_function to calculate and display the current download speed in MB/s.
    
    - This provides the user with real-time feedback on download performance.

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from pytube import YouTube
+import time
+
+previous_bytes = 0
+time_marker = time.time()
 
 
 def progress_function(stream, chunk, bytes_remaining):
+    global previous_bytes, time_marker
+
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
+
+    # Calculate download speed
+    elapsed_time = time.time() - time_marker
+    bytes_per_second = (bytes_downloaded - previous_bytes) / elapsed_time
+    speed = f"{bytes_per_second / (1024 ** 2):.2f} MB/s"  # Convert to MB/s for display
+
     percentage_of_completion = bytes_downloaded / total_size * 100
 
     bar_length = 50
     filled_bar = int(percentage_of_completion * bar_length / 100)
     bar = '=' * filled_bar + ' ' * (bar_length - filled_bar)
 
-    print(f"\r[{bar}] Downloading... {percentage_of_completion:.2f}% complete", end='')
+    print(f"\r[{bar}] Downloading... {percentage_of_completion:.2f}% complete at {speed}", end='')
+
+    # Reset for next interval
+    previous_bytes = bytes_downloaded
+    time_marker = time.time()
 
 
 def download_video(url, save_path="."):
